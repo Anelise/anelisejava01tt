aad4e1ccfd7123719b99fb9b2705d556c0687edf
aluno03 sala04 <aluno03sala04@targettrust.local>
2019-05-04 16:29:53 -0300

    adicionada classe Main e classe nota

--- meuprimeiroprojeto/src/Main.java
+++ meuprimeiroprojeto/src/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public float calcularMediaNota(Nota nota1, Nota nota2){
+        return (nota1.getValor() + nota2.getValor())/2;
+    }
 }
---
+++ meuprimeiroprojeto/src/Nota.java
@@ -0,0 +1,11 @@
+public class Nota {
+    private float valor;
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+}
