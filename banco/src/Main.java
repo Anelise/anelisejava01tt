f7fc22d0eb7f9333e55d1161c1e032957c9afff7
viniantunes <vinicius.mantunes@gmail.com>
2019-05-11 17:07:01 -0300

    criando banco

--- banco/src/Main.java
+++ banco/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        /** expressoes lambda
+         * pessoaLista.forEach( pessoa->{
+         * system.out
+         *});
+         */
     }
 }
